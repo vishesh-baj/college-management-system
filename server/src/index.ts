f23bf75f0dab3f6f4c725daaa385719640d24683
vishesh-baj <vishesh.bajpayee@icloud.com>
2024-05-13 23:03:34 +0530

    added initial projects for client and server

---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+const MONGO_DB_URI = process.env.MONGO_DB_URI;
+if (!MONGO_DB_URI) {
+  throw new Error("MONGO DB URI not provided");
+}
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// db connection
+mongoose
+  .connect(MONGO_DB_URI)
+  .then(() => {
+    console.log("DB CONNECTED");
+    app.listen(PORT, () => console.log(`SERVER STARTED AT PORT: ${PORT}`));
+  })
+  .catch((error) => console.error("ERROR OCCURED: ", error));
+
+// routes
